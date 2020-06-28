4147e6340967ae8fececfba9f7f416d11d94242c
Pranjalya <pranjalyawarrior@gmail.com>
2020-06-28 15:17:09 +0530

    lines updated

--- LogExtractor.py
+++ LogExtractor.py
@@ -4,30 +4,50 @@ import argparse
 import subprocess
 import time
 import dateutil.parser
+import linecache
+import mmap
+
+def fileLinecount(filename):
+    out = subprocess.Popen(['wc', '-l', filename],
+                         stdout=subprocess.PIPE,
+                         stderr=subprocess.STDOUT
+                         ).communicate()[0]
+    return int(out.partition(b' ')[0])
+
+def getTail(filename, n):
+    """Returns last n lines from the filename. No exception handling"""
+    size = os.path.getsize(filename)
+    with open(filename, "r") as f:
+        # for Windows the mmap parameters are different, so set them according to system
+        fm = mmap.mmap(f.fileno(), 0, mmap.MAP_SHARED, mmap.PROT_READ)
+        try:
+            for i in range(size - 1, -1, -1):
+                if fm[i] == '\n':
+                    n -= 1
+                    if n == -1:
+                        break
+            return fm[i + 1 if i else 0:].splitlines()
+        finally:
+            fm.close()
 
 def getLine(file:str, stamp, loc) -> int:
-    '''
-
-    '''
-    linecounter = 'wc -l logs/{}'.format(file)
     left = 1
-    right = int(subprocess.run(linecounter.split(), stdout=subprocess.PIPE).stdout.decode('utf-8').split()[0])
+    right = fileLinecount(loc+"/"+file)
     last = right
 
     # To check if our stamp is lesser than the first stamp
-    stmt = 'head -n 1 logs/{}'.format(file)
-    if(stamp < dateutil.parser.parse(subprocess.run(stmt.split(), stdout=subprocess.PIPE).stdout.decode('utf-8').split()[0])):
+    if(stamp < dateutil.parser.parse(linecache.getline(loc+"/"+file, 1).split(",")[0])):
         return 1
 
-    # Binary Searching for the starting stamp just greater than given stamp
+    # Binary Search for the starting stamp just greater than given stamp
     while left <= right:
         mid = left + (right-left)//2
         if mid==1 or mid==last:
             return mid
         else:
             stmtmid = "sed '{}p;{}p;{}q;d' log/{}".format(mid-1, mid, mid+1, file)
-            midstamps = subprocess.run(stmtmid.split(), stdout=subprocess.PIPE).stdout.decode('utf-8').split('\n')
-            midstamps = list(map(lambda x: dateutil.parser.parse(x.split()[0]), midstamps[:3]))
+            midstamps = subprocess.run(stmtmid.split(), stdout=subprocess.PIPE, shell=True).stdout.decode('utf-8').split('\n')
+            midstamps = list(map(lambda x: dateutil.parser.parse(x.split(",")[0]), midstamps[:3]))
             if(stamp >= midstamps[1] and stamp<=midstamps[2]):
                 return mid
             elif(stamp<=midstamps[1] and stamp>=midstamps[0]):
@@ -43,17 +63,19 @@ def showLogs(startfile:str, endfile:str, start, end, loc:str) -> None:
     startline = getLine(startfile, start, loc)
     endline = getLine(endfile, end, loc)
 
-    startlinecounter = 'wc -l {}/{}'.format(loc, startfile)
-    endlinecounter = 'wc -l {}/{}'.format(loc, endfile)
-
-    startlinecount = int(subprocess.run(startlinecounter.split(), stdout=subprocess.PIPE).stdout.decode('utf-8').split()[0])
-    endlinecount = int(subprocess.run(endlinecounter.split(), stdout=subprocess.PIPE).stdout.decode('utf-8').split()[0])
+    startlinecount = fileLinecount(loc+"/"+startfile)
     
     # Display last total lines - initial stamp line lines
-    cmd = 'tail -n {} {}/{}'.format(startlinecounter-startlinecount+1, loc, startfile)
-    os.system(cmd)
+    print(getTail(loc+"/"+startfile, startlinecount-startline))
 
-    ## TO DO : Extract no and show all in between
+    # Display all lines of files between start file and end file
+    for fileno in range(int(startfile[7:13])+1, int(endfile[7:13])):
+        with open(loc+"/LogFile{}.log".format(fileno)) as fi:
+            print(fi.readline())
+    
+    # Display first lines of end file
+    print(linecache.getline(loc+endfile, endline))
+    
 
 def getLogs(start:str, end:str, loc:str) -> None:
     start = dateutil.parser.parse(start)
@@ -64,17 +86,17 @@ def getLogs(start:str, end:str, loc:str) -> None:
     if(start > end):
         raise ArithmeticError
 
+    startfound = False
     for f in os.listdir(loc):
-        stmthead = 'head -n 1 {}/{}'.format(loc, f)
-        stmttail = 'tail -n 1 {}/{}'.format(loc, f)
-        head = subprocess.run(stmthead.split(), stdout=subprocess.PIPE).stdout.decode('utf-8').split()[0]
-        tail = subprocess.run(stmttail.split(), stdout=subprocess.PIPE).stdout.decode('utf-8').split()[0]
+        head = linecache.getline(loc+"/"+f, 1).split(",")[0]
+        tail = getTail(loc+"/"+f, 1)[0].split(",")[0]
         header = dateutil.parser.parse(head)
         tailer = dateutil.parser.parse(tail)
-        if(header <= start and tailer >= start):
+        if(not startfound and tailer >= start):
             print("Head : ", head, " Tail : ", tail, "")
             startfile = f
-        if(header <= end and tailer >= end):
+            startfound = True
+        if(tailer >= end):
             print("Head : ", head, " Tail : ", tail, "")
             endfile = f
             break
